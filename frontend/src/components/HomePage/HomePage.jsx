f1b6dad6a0d19365c867caf27dccccfffc36f451
YassineCH-7777 <ahmedmhmd1912@gmail.com>
2025-01-30 16:25:44 +0100

    ok

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:810dfdb4c015f32cda42f46e80de39dc3b847d9476477d68bfb50c81fdd846a0
-size 3524
+oid sha256:05f171f25c8850cf39f753302477fb296de82a738e07e7b39db48ecd5d787aa5
+size 3523
