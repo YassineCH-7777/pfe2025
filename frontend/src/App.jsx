a58b188b5c7923453035ca69cde104beafea7c40
YassineCH-7777 <ahmedmhmd1912@gmail.com>
2025-02-01 14:33:09 +0100

    ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,3 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
-import Footer from './components/Footer/Footer';
-import HomePage from './components/HomePage/HomePage';
-import AboutPage from './components/AboutPage/AboutPage';
-import PanelInputForm from './components/PanelInputForm/PanelInputForm';
-import './index.css';
-import Header from './components/Header/Header';
-
-function App() {
-  const handleOptimize = (newLayout, panels, toolParams) => {
-    console.log('Optimization result:', newLayout);
-  };
-
-  return (
-    <Router>
-      <div>
-        {/* Barre de navigation */}
-        <Header />
-        
-        {/* Définition des routes */}
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/optimise" element={<PanelInputForm onOptimize={handleOptimize} />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-
-        {/* Footer */}
-        <Footer />
-      </div>
-    </Router>
-  );
-}
-
-export default App;
+version https://git-lfs.github.com/spec/v1
+oid sha256:b295e453c4ffac3e4390434241b62dce4d8a8cf6033ac54a03558379b8ea7071
+size 1003
