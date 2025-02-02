67b7c394dd44cd0e28d921b89043eac4662b98e5
YassineCH-7777 <ahmedmhmd1912@gmail.com>
2025-02-02 04:30:46 +0100

    ok

--- frontend/src/pages/WoodPage/WoodPage.jsx
+++ frontend/src/pages/WoodPage/WoodPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb94a52e87fb7f558ca72285a8df14c37aeb6c0ca81f145ab47fa9d1832db7ac
-size 4066
+oid sha256:8d923716cc12738648825d8df2f9cc042ba6977ce32eac40a302ffa89b2fa494
+size 4237
