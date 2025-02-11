1f423aac7a4441a2342b1f467ed1511340463c3c
YassineCH-7777 <ahmedmhmd1912@gmail.com>
2025-02-11 13:53:30 +0100

    ok

--- backend/output/cutting_plan.gcode
+++ backend/output/cutting_plan.gcode
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4bf7bc088667473c779677c0d7aa11c956ab542aebdabcdb7effffd4e06dea7
-size 1680
+oid sha256:2b4fd0cf344e3727f30bc560bf0cd7087f81c593ddcd0b3b347de1d20d6953c2
+size 2003
--- frontend/src/components/ProjectDetails/ProjectDetails.jsx
+++ frontend/src/components/ProjectDetails/ProjectDetails.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f78d424554081aa3257aeab34ef852f5cf59593a476547e5518aaf981818d39a
-size 3806
+oid sha256:244ae32d22890ef6eea395b8e6f6db562f5981166d4170bd94e34aadc27aeba4
+size 3810
