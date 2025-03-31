b95d33065ee62bb6eee015baedf64a1c9767cad2
YassineCH-7777 <ahmedmhmd1912@gmail.com>
2025-03-31 02:06:19 +0000

    ok

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:050f95013251fd82ae7919c509ea7fe565c3141185fd790edc2050eee97a8d39
-size 3891
+oid sha256:72d43eeaedf8ae89252cae85cf9731c8e03df3ce08e4071bb6fc4b81e137e705
+size 4147
--- frontend/src/components/ProjectDetails/ProjectDetails.jsx
+++ frontend/src/components/ProjectDetails/ProjectDetails.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2d0f2aad0982954db3db2fc81aeefb0c4e15b086c2b313986e8e3686e307b2e
-size 3942
+oid sha256:82aee43f9c43f1fb1ca445e8cd19ed223ca4347690393a6d6bca848bba76134f
+size 4013
