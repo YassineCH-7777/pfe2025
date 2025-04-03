6c67adba07556e432fd7e718e101f36c964adfb2
YassineCH-7777 <ahmedmhmd1912@gmail.com>
2025-04-03 19:57:27 +0000

    ok

--- frontend/src/pages/WoodDetailPage/WoodDetailPage.jsx
+++ frontend/src/pages/WoodDetailPage/WoodDetailPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73af742747f291d5de49766b4313c71e5b922882f14b821b3c55ea28966988aa
-size 14808
+oid sha256:bcd42cb11913f1cacef2f11e61636ae7e16a589aebdae6834207f83b99d2b798
+size 14833
