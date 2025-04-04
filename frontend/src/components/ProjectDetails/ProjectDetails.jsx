edab8f3fbfe09bb4cd22e6f4cb37b86022616f61
YassineCH-7777 <ahmedmhmd1912@gmail.com>
2025-04-04 00:20:21 +0000

    ok

--- frontend/src/components/ProjectDetails/ProjectDetails.jsx
+++ frontend/src/components/ProjectDetails/ProjectDetails.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09dea939afc93598e3aa27bcaf29a02c6ef30d2ff70eccc75d585bd9504dcdb1
-size 7832
+oid sha256:f43130ffc5f0594a80f9bb6b93a7a1cbf1ac54027c0998f84f7808d24f8c098d
+size 8057
